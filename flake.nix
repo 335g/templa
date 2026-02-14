e56cec1876b96379057ce950b69ff4652569ccb8
335g <pub335g@gmail.com>
2026-02-14 11:34:03 +0900

    feat: rust template

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  outputs = inputs: {
+    templates = {
+      rs = {
+        path = "./rust";
+        description = "";
+      };
+    };
+  };
+}
---
+++ rust/flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    crane.url = "github:ipetkov/crane/v0.21.3";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { pkgs, system, ... }:
+        let
+          rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustToolchain;
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+            ];
+          };
+
+          devShells.default = craneLib.devShell {
+            packages = with pkgs; [
+              taplo
+              sccache
+              clang
+              mold
+            ];
+
+            env = {
+              RUSTC_WRAPPER = "sccache";
+            };
+          };
+        };
+    };
+}
